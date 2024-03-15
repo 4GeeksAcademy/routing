cb18868c587b1dc9acaa32ad06512078f5903e93
cristiang83 <156969890+cristiang83@users.noreply.github.com>
2024-03-15 19:11:13 -0400

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import { CounterContainer } from "@/componentes/CounterContainer";
+import { Button, Container, Row, Col} from "react-bootstrap";
+
+
+
+
+export default function () {
+  return (
+    <Container>
+    <CounterContainer/>
+ </Container>
+  )
+}
